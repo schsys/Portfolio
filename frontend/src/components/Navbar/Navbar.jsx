93177071d0e9d0343d25bf12872b9c61efd2b1ec
Luciano Schmarsow <78238612+clarkos@users.noreply.github.com>
2023-04-07 23:11:35 +0100

    fixed nav

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -15,12 +15,12 @@ const Navbar = () => {
         {["home", "about", "work", "skills", "contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
-            <a href="https://www.dropbox.com/s/zhboifwigzle4cv/FullStackDev_EN.pdf?dl=0" download="ldsch-resume.pdf">resume</a>
+            <a href={`#${item}`}>{item}</a>
           </li>
         ))}
         <li className="app__flex p-text">
             <div />
-            <a href={`#${item}`}>{item}</a>
+            <a href="https://github.com/clarkos/Portfolio/blob/main/frontend/public/ldsch-resume.pdf" download>resume</a>
           </li>
       </ul>
       <div className="app__navbar-menu">
